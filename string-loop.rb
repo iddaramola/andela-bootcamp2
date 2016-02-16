e31417f856acb55a74f6c39cf858a40bac5cd93c
Idowu Daramola <idowu.daramola@gmail.com>
2016-02-16 13:43:23 +0000

    my fouth commit, changed each iterator to for loop

--- string-loop.rb
+++ string-loop.rb
@@ -2,4 +2,7 @@ puts "enter your name"
 name = gets.chomp
 #name.each_char{|c| puts c.upcase}
 name_array = name.split(//)
-name_array.each{|c| puts "#{name_array.index(c)}: #{c}"}
+#name_array.each{|c| puts "#{name_array.index(c)}: #{c}"}
+for x in name_array
+  puts x
+end  
