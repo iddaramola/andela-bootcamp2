4ff16ce46bbc57594de3a73c935cba1ab309996d
Idowu Daramola <idowu.daramola@gmail.com>
2016-02-16 13:19:48 +0000

    my third commit

--- string-loop.rb
+++ string-loop.rb
@@ -1,3 +1,5 @@
 puts "enter your name"
 name = gets.chomp
-name.each_char{|c| puts c.upcase}
+#name.each_char{|c| puts c.upcase}
+name_array = name.split(//)
+name_array.each{|c| puts "#{name_array.index(c)}: #{c}"}
